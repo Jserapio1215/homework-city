ab24072d5032663ffca95a336581130ae53b3be4
Jserapio1215 <jon.serapio@gmail.com>
2016-03-27 20:20:59 -0700

    Updated Final Submit

--- js/index.js
+++ js/index.js
@@ -9,9 +9,8 @@ $(document).ready(function() {
 		
 		var city = $('#city-type').val(); 
 		//created a variable to log what city is inputted when someone clicks submit
-
-		event.preventDefault();
-	   	// adding event.prevent will stop a form submission from occuring
+		var body = $('body');
+		
 
 		// create if else statements to when someone inputs a city name, to determine what background picture will be used
 		// If a user submits the following:
@@ -21,27 +20,31 @@ $(document).ready(function() {
 	    // "Austin" or "ATX" THEN make the background of the page austin.jpg
 	    // "Sydney" or "SYD" THEN make the background of the page sydney.jpg
 	    
-	    if (city === "New York" || "New York City" || "NYC") {
+	    if (city === "New York" || city === "New York City" || city === "NYC") {
 	   	removeClass();
 	   	body.addClass('nyc');
 	   	}
-	    else if (city === "San Francisco" || "SF" || "Bay Area") {
+	    else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
 	   	removeClass();
 	   	body.addClass('sf');
 	   	}
-		else if (city === "Los Angeles" || "LA" || "LAX" ) {
+		else if (city === city === "Los Angeles" || city === "LA" || city === "LAX" ) {
 		removeClass();
 		body.addClass('la');
 		}
-		else if (city === "Austin" || "ATX") {
+		else if (city === "Austin" || city === "ATX") {
 	   	removeClass();
 	   	body.addClass('austin');
 	   	}
-	   	else if (city === "Sydney" || "SYD") {
+	   	else if (city === "Sydney" || city === "SYD") {
 	   	removeClass();
 	   	body.addClass('sydney');
 	   	}
+
+	   	event.preventDefault();
+	   	// adding event.prevent will stop a form submission from occuring
 	   	
+	   	   		   	
 	   	// in order to change the background image to a new image, creating a removeClass function will serve this purpose
 	   	function removeClass () {
 		$('body').removeClass();
