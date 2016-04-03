f56b5be88d193ac5af960cf27eda46a69c1661e6
Jserapio1215 <jon.serapio@gmail.com>
2016-04-03 10:07:20 -0700

    updated JS and CSS

--- js/citipixindex2.js
+++ js/citipixindex2.js
@@ -9,5 +9,23 @@ $(document).ready(function() {
 	$('#city-type').on('change', function() {
 		var value= this.value;
 		console.log(value);
+		// if states are used to change the background when a specific value is selected from the Array
+		if (value === "NYC") {
+		$('body').attr('class', '.nyc');
+		}
+		else if (value === "SF") {
+			$('body').attr('class', '.sf');
+		}
+		else if (value === "LA") {
+			$('body').attr('class', '.la');
+		}
+		else if (value === "ATX") {
+			$('body').attr('class', '.austin');
+		}
+		else if (value === "SYD") {
+			$('body').attr('class', '.sydney');
+		}
 	})
 })	
+
+// used $.change to change the background image 
